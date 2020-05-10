3c47775b9ede3b93dfd5d310f6cc8a8231b44de1
NicholasAS <anthony.nicholas84@gmail.com>
2020-05-10 21:47:54 +0700

    add vue config .js

---
+++ frontend/vue.config.js
@@ -0,0 +1,34 @@
+/* 
+    konfigurasi agar dapat memakai relative image
+    https://bootstrap-vue.js.org/docs/reference/images/
+*/
+module.exports = {
+    devServer : {
+      proxy:"http://localhost:3000",
+    },
+    configureWebpack : {
+      devtool : 'source-map'
+    },
+    chainWebpack: config => {
+      config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          options.transformAssetUrls = {
+            img: 'src',
+            image: 'xlink:href',
+            'b-avatar': 'src',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'src',
+            'b-card-img-lazy': ['src', 'blank-src'],
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+          }
+  
+          return options
+        })
+    }
+  }

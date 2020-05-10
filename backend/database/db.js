8de9c1d05f57e1430b906e99818703f8dc966580
NicholasAS <anthony.nicholas84@gmail.com>
2020-05-10 09:39:10 +0700

    initial commit

---
+++ backend/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  "mongodb+srv://nicho:1234@chatapplication-yq6bn.mongodb.net/test",
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB connection succeeded");
+    } else {
+      console.log("Error in DB Connection " + err);
+    }
+  }
+);
+
+module.export = mongoose;
---
+++ backend/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  conversations: {
+    type: Array,
+  },
+  friendsListUsername: {
+    type: Array,
+  },
+});
+
+var User = mongoose.model("User", UserSchema);
+
+module.exports = User;

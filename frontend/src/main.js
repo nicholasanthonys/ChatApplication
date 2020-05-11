68575323aca56e15394014c3f89b4f9f761f3f75
NicholasAS <anthony.nicholas84@gmail.com>
2020-05-11 20:50:06 +0700

    use VueSocketIO Object

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router";
 
+import VueSocketIO from "vue-socket.io";
+import SocketIO from "socket.io-client";
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: SocketIO("http://localhost:3000"),
+  })
+);
+
 Vue.config.productionTip = false;
 
 new Vue({

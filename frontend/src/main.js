3234f7c2b1972c73560359da4e600e9b11e5517f
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-05-13 19:54:10 +0700

    use vuelidate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router";
 
+//transmitting between chat
 import VueSocketIO from "vue-socket.io";
 import SocketIO from "socket.io-client";
+//ui library
 import vuetify from "./plugins/vuetify";
+//scrolling
 import PerfectScrollbar from "vue2-perfect-scrollbar";
 import "vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css";
+//icon material
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// for validation form
+import Vuelidate from 'vuelidate'
+
+// for validation form
+Vue.use(Vuelidate)
 
 Vue.use(PerfectScrollbar);
 

96a1551ada334d5181b75bdaac9ac783b8fae1c1
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-05-14 17:57:37 +0700

    rename request api, add loadListChat function

--- frontend/src/services/conversation.js
+++ frontend/src/services/conversation.js
@@ -2,16 +2,26 @@ import axios from "axios";
 
 export const getConversation = async (id) => {
   //id adalah uid object conversation
-  console.log('from axios, id is ' + id);
-  return axios.get("/conversation/" + id);
+  console.log("from axios, id is " + id);
+  return axios.post("/conversation/chatscreen", {
+    id: id,
+  });
 };
 
 //send Message
 export const sendMessage = async (id, sender, receiver, message) => {
   //id adalah uobject conversation
-  return axios.post("/conversation/" + id, {
+  return axios.post("/conversation/detail", {
+    id: id,
     sender: sender,
     receiver: receiver,
     message: message,
+  });
+};
+
+//load list chat with lates message
+export const loadListChat = async (username) => {
+  return axios.post("/conversation/listchat", {
+    username: username,
   });
 };

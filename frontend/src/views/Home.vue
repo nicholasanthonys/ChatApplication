8aacf11e2918a610e6eef686b0df4f0d2628d27c
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-05-14 17:58:14 +0700

    add listChat screen

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="home">
-    <ChatScreen v-bind:user = "user" />
+    <p>{{id}}</p>
+    <div class="row">
+      <div class="col">
+        <ListChat v-bind:user="user" v-on:chat-clicked="chatClicked" />
+      </div>
+      <div class="col">
+        <ChatScreen v-bind:user="user" v-bind:id="id" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ChatScreen from "@/components/ChatScreen";
+import ListChat from "@/components/ListChat";
 export default {
   name: "Home",
   components: {
-    ChatScreen
+    ChatScreen,
+    ListChat
   },
-  data(){
+  data() {
     return {
-      user : this.$session.get("user")
+      user: this.$session.get("user"),
+      id: ""
+    };
+  },
+  methods: {
+    chatClicked(value) {
+      this.id = value;
     }
   },
   beforeCreate: function() {
@@ -23,3 +39,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+#home {
+  padding-left: 50px;
+  padding-right: 50px;
+}
+</style>

652fcf2c9a06809950e3edce5ade9783451f6d20
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-05-14 17:57:48 +0700

    add getOtherUser function

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -1,11 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const getUser = async (email,password)=>{
-    return axios.post('/users/login',{
-        email : email,
-        password : password
-    });
+export const getUser = async (email, password) => {
+  return axios.post("/users/login", {
+    email: email,
+    password: password,
+  });
 };
 
-
-
+export const getOtherUser = async (username) => {
+  return axios.post("/users/getotheruser", {
+    username: username,
+  });
+};

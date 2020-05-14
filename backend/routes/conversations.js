2a0be604a7d084747c0f37ee028955c1e2c9bcb6
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-05-14 17:56:19 +0700

    rename api

--- backend/routes/conversations.js
+++ backend/routes/conversations.js
@@ -8,14 +8,19 @@ router.post('/new',function(req,res){
 });
 
 //get conversation based on id 
-router.get('/:id',function(req,res){
-    console.log('getConversation id is ' + req.params.id);
-    conversationController.getConversation(req,res,req.params.id);
+router.post('/chatscreen',function(req,res){
+
+    console.log('getConversation id is ' + req.body.id);
+     conversationController.getConversation(req,res);
 });
 
 //send message to a conversation
-router.post('/:id',function(req,res){
-    conversationController.insertMessage(req,res,req.params.id);
+router.post('/detail',function(req,res){
+    conversationController.insertMessage(req,res);
+});
+
+router.post('/listchat',function(req,res){
+   conversationController.loadListChat(req,res);
 });
 
 
